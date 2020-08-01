94af2fc20e3e58d9a86e33bd4f456524555171a6
Lola <lola@egherman.com>
2020-08-01 11:39:51 -0500

    implement getEvent

--- src/utils/gcal.js
+++ src/utils/gcal.js
@@ -22,25 +22,8 @@ export const getEvents = async() => {
     })
   return events
 };
-export function getEvent (callback, eventId) {
+export const getEvent = async(eventId) => {
   let url = `https://www.googleapis.com/calendar/v3/calendars/${serverRuntimeConfig.gcal.calendarID}/events/${eventId}?key=${serverRuntimeConfig.gcal.apiKey}`
-  axios
-    .get(url)
-    .end((err, resp) => {
-      if (!err) {
-        // create array to push events into
-        const events = []
-        // in practice, this block should be wrapped in a try/catch block,
-        // because as with any external API, we can't be sure if the data will be what we expect
-        JSON.parse(resp.text).items.map((event) => {
-          events.push({
-            // an event from Google Calendar can be a full day event or a regular one
-            start: event.start.date || event.start.dateTime,
-            end: event.end.date || event.end.dateTime,
-            title: event.summary,
-          })
-        })
-        callback(events)
-      }
-    })
-}
+  const result = await axios.get(url)
+  return result.data
+};
