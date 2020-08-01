959da24ad68505acde306bf612a4120876c6b228
Lola <lola@egherman.com>
2020-08-01 13:22:15 -0500

    implement cache for events

--- src/utils/gcal.js
+++ src/utils/gcal.js
@@ -7,29 +7,28 @@ const cache = new LruCache({ max: 60, maxAge: 60 * 1000 });
 const { serverRuntimeConfig } = getConfig();
 
 export const getEvents = async() => {
-  let url = `https://www.googleapis.com/calendar/v3/calendars/${serverRuntimeConfig.gcal.calendarID}/events?key=${serverRuntimeConfig.gcal.apiKey}`
-  const result = await axios.get(url)
-  const events = []
-  result.data.items
-    .map((event) => {
-      const [title, type] = event.summary.split(/:\s+/, 2).reverse();
-      events.push({
-        Date: event.start.dateTime,
-        Title: title || 'TBD',
-        Type: type || 'Event',
-        Link: event.htmlLink || '',
-        Description: event.description || '',
-        Location: event.location || 'TBD',
-        id: event.id,
+  if (!cache.has('events')) {
+    let url = `https://www.googleapis.com/calendar/v3/calendars/${serverRuntimeConfig.gcal.calendarID}/events?key=${serverRuntimeConfig.gcal.apiKey}`
+    const result = await axios.get(url)
+    const events = []
+    result.data.items
+      .map((event) => {
+        const [title, type] = event.summary.split(/:\s+/, 2).reverse();
+        events.push({
+          Date: event.start.dateTime,
+          Title: title || 'TBD',
+          Type: type || 'Event',
+          Link: event.htmlLink || '',
+          Description: event.description || '',
+          Location: event.location || 'TBD',
+          id: event.id,
+        })
       })
-    })
-  return events
+    cache.set('events', events)
+  }
+  return cache.get('events')
 };
 export const getEvent = async(eventId) => {
-  let url = `https://www.googleapis.com/calendar/v3/calendars/${serverRuntimeConfig.gcal.calendarID}/events/${eventId}?key=${serverRuntimeConfig.gcal.apiKey}`
-  const result = await axios.get(url)
-  let event = result.data
-  return {
-
-  }
+  let events = await getEvents()
+  return events.filter(event => event.id === eventId)[0];
 };
