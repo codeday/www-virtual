a50587d7d4553ba25c912f8592af412501531373
Tyler Menezes <github@xanadu.ws>
2020-08-10 13:11:07 -0400

    Fix error logging on schedule page

--- src/pages/schedule/index.js
+++ src/pages/schedule/index.js
@@ -1,17 +1,19 @@
+import React from 'react';
 import moment from 'moment-timezone';
 import Content from '@codeday/topo/Molecule/Content';
 import Text, { Link } from '@codeday/topo/Atom/Text';
+import getConfig from 'next/config';
 import Page from '../../components/Page';
 import Calendar from '../../components/Calendar';
 import { getEvents } from '../../utils/gcal';
-import getConfig from 'next/config';
+
 const { publicRuntimeConfig } = getConfig();
 export const getServerSideProps = async () => {
   let calendar = null;
   try {
     calendar = await getEvents();
   } catch (err) {
-    console.error('airtable is down (again) and the cache is empty.');
+    console.error(err);
   }
   return {
     props: {
@@ -21,7 +23,19 @@ export const getServerSideProps = async () => {
 };
 
 export default function Home({ calendar }) {
-  const calendarHydrated = calendar.map((e) => ({ ...e, Date: moment(e.Date)}));
+  const calendarHydrated = calendar.map((e) => ({ ...e, Date: moment(e.Date) }));
+
+  if (calendar.length === 0) {
+    return (
+      <Page slug="/schedule" title="Schedule">
+        <Content>
+          <Text mb={16}>
+            The schedule is not currently available. Check back soon.
+          </Text>
+        </Content>
+      </Page>
+    );
+  }
 
   return (
     <Page slug="/schedule" title="Schedule">
