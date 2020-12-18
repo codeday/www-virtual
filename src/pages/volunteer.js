f83d7c064bb6775fa7585dc6bfecb48f29f2d69e
Bobcat <bobcat4848@gmail.com>
2020-12-18 15:05:54 -0600

    Updating Volunteer Page

--- src/pages/volunteer.js
+++ src/pages/volunteer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { apiFetch } from '@codeday/topo/utils';
 import Content from '@codeday/topo/Molecule/Content';
+import Image from '@codeday/topo/Atom/Image';
 import Box, { Grid } from '@codeday/topo/Atom/Box';
 import Text, { Heading } from '@codeday/topo/Atom/Text';
 import Button from '@codeday/topo/Atom/Button';
@@ -11,8 +12,21 @@ import FaqAnswer from '../components/FaqAnswer';
 export default function Volunteer({ faqs }) {
   return (
     <Page slug="/volunteer" title="Volunteer">
+      <Image
+          src="https://img.codeday.org/o/m/f/mfwgeuyxb7euvouiwx5252n79xan15ujhprpjqh1q198s7uutheambb6eamm2zdyu1.jpg"
+          rounded="md"
+          m="auto"
+          mt={-8}
+          mb={4}
+          alt=""
+        />
       <Content>
-        <Heading as="h2">Volunteer for Virtual CodeDay</Heading>
+        <Heading as="h2">Make a Difference. Volunteer for Virtual CodeDay.</Heading>
+        <Text>Current positions open: </Text>
+        <Box m="auto" textAlign="left">
+          <Button onClick={smoothScroll} variant="solid" variantColor="red" size="lg" mt={2}>Volunteer Now!</Button>
+        </Box>
+
       </Content>
       {faqs.length > 0 && (
         <Content paddingBottom={8}>
@@ -28,13 +42,20 @@ export default function Volunteer({ faqs }) {
           More questions? <Button as="a" href="mailto:team@codeday.org">Contact us!</Button>
         </Content>
       )}
-      <Content>
+      <Content id="register">
         <Heading as="h3" fontSize="2xl">Apply</Heading>
         <CognitoForm formId={63} />
       </Content>
     </Page>
   );
 };
+
+function smoothScroll() {
+  document.querySelector('#register').scrollIntoView({
+      behavior: 'smooth'
+  });
+}
+
 
 const query = `{
   cms {
