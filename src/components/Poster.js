ab3fececf0f0f7df473b7060bf64cc95df30400e
Lola <lola@egherman.com>
2021-05-27 14:02:11 -0500

    use contentfulBaseUrl for posters

--- src/components/Poster.js
+++ src/components/Poster.js
@@ -9,7 +9,7 @@ import Image from "@codeday/topo/Atom/Image";
 
 export default function Poster({ poster, ...props }) {
     return (
-        <Box as="a" bg="gray.100" borderRadius={10} download={"codeday_" + poster.fileName} href={poster.url} {...props}>
+        <Box as="a" bg="gray.100" borderRadius={10} download={"codeday_" + poster.fileName} href={poster.contentfulBaseUrl} {...props}>
             <Image key={poster.url} src={poster.url} borderRadius={10} fallback={<Skelly/>}/>
             </Box>
     )
